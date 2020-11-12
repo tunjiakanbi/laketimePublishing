01b7df991f6dd509f6cf413c298799e4ff27bd35
Tunji Akanbi <you@example.com>
2020-11-12 11:23:37 -0600

    added new css rules

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-  });
+      // Look for .hamburger
+      var hamburger = document.querySelector(".hamburger");
+      // On click
+      hamburger.addEventListener("click", function () {
+        // Toggle class "is-active"
+        hamburger.classList.toggle("is-active");
+        // Do something else, like open/close menu
+      });
+
+      $(document).ready(function () {
+
+        // custom javascript starts here
+        // $('.navbar-nav>li>a').on('click', function(){
+        //   $('.navbar-collapse').collapse('hide');
+        // });
+
+        // document ready ends below
+      });
